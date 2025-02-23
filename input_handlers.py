f8f563faba4257284b334c852d8bfed79a79e8b9
Colin <kappa6708@gmail.com>
2025-02-23 17:12:20 -0600

    Part 2 done.

--- input_handlers.py
+++ input_handlers.py
@@ -5,12 +5,10 @@ import tcod.event
 from actions import Action, EscapeAction, MovementAction
 
 class EventHandler(tcod.event.EventDispatch[Action]):
-
-    def ev_quit(self, event:tcod.event.Quit) -> Optional[Action]:
+    def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
         raise SystemExit()
 
-
-    def ev_keydown(selfself, event: tcod.event.KeyDown) -> Optional[Action]:
+    def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
         action: Optional[Action] = None
 
         key = event.sym
@@ -18,14 +16,17 @@ class EventHandler(tcod.event.EventDispatch[Action]):
         if key == tcod.event.K_UP:
             action = MovementAction(dx=0, dy=-1)
         elif key == tcod.event.K_DOWN:
-            action == MovementAction(dx=0, dy=1)
+            action = MovementAction(dx=0, dy=1)
         elif key == tcod.event.K_LEFT:
-            action == MovementAction(dx=-1, dy=0)
+            action = MovementAction(dx=-1, dy=0)
         elif key == tcod.event.K_RIGHT:
-            action == MovementAction(dx=0, dy=-1)
+            action = MovementAction(dx=1, dy=0)
 
         elif key == tcod.event.K_ESCAPE:
             action = EscapeAction()
+
+        # No valid key was pressed
+        return action
 
         #No valid key was pressed
         return action
